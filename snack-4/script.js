714fa6a4c6fc9f9f775cdd8009f74f44f49f0464
GianniC88 <gianni.colucci.88@gmail.com>
2025-06-13 17:16:59 +0200

    Correggi la sintassi e migliora la stampa dei nomi degli autori utilizzando map, forEach e ciclo for

--- snack-4/script.js
+++ snack-4/script.js
@@ -1,9 +1,28 @@
 const posts = [
-  { author: 'Marco', date: '12/3/2021', content: 'lorem ipsum...' },
-  { author: 'Luca', date: '30/6/2021', content: 'lorem ipsum...' },
-  { author: 'Fabrizio', date: '12/9/2020', content: 'lorem ipsum...' },
-  { author: 'Enrico', date: '09/12/2021', content: 'lorem ipsum...' },
-]
+  { author: "Marco", date: "12/3/2021", content: "lorem ipsum..." },
+  { author: "Luca", date: "30/6/2021", content: "lorem ipsum..." },
+  { author: "Fabrizio", date: "12/9/2020", content: "lorem ipsum..." },
+  { author: "Enrico", date: "09/12/2021", content: "lorem ipsum..." },
+];
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+
+const nomeAutore = posts.map((element, index, arr) => {
+  return element.author;
+});
+console.log(nomeAutore);
+
+const nomiAutori = [];
+posts.forEach((element, index, arr) => {
+  nomeAutore.push(element.author);
+});
+console.log(nomiAutori);
+//metodo FOR
+
+const nome = [];
+for (let i = 0; 1 < posts.length; i++) {
+  const naME = posts[i];
+  nome.push(naME.author);
+}
+console.log(nome);
