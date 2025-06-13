a4586740083aa9665f7747db514b0454b87e5eac
GianniC88 <gianni.colucci.88@gmail.com>
2025-06-13 16:38:06 +0200

    terzo snack

--- snack-3/script.js
+++ snack-3/script.js
@@ -2,3 +2,27 @@ const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
+
+//ciclo for
+const numb = [];
+for (let i = 0; i < numbers.length; i++) {
+  const num = numbers[i];
+  numb.push(num + 1);
+}
+console.log(numb);
+
+//ciclo forEach
+
+const numeriDue = [];
+
+numbers.forEach((elemnt, index, arr) => {
+  numeriDue.push(elemnt + 1);
+});
+console.groupCollapsed(numeriDue);
+
+//ciclo map
+
+const nuMeri = numbers.map((element, index, arr) => {
+  return element + 1;
+});
+console.log(nuMeri);
