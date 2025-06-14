4b71420e9ddca7789506d6e84452e2bf6eaed0b9
GianniC88 <gianni.colucci.88@gmail.com>
2025-06-14 21:01:51 +0200

    snack 6 ciclo FOR

--- snack-6/script.js
+++ snack-6/script.js
@@ -1,14 +1,34 @@
 const zucchine = [
-  { type: 'Napoletana', weight: 10, length: 4 },
-  { type: 'Trombetta', weight: 13, length: 16 },
-  { type: 'Napoletana', weight: 4, length: 23 },
-  { type: 'Trombetta', weight: 11, length: 6 },
-  { type: 'Napoletana', weight: 2, length: 17 },
-  { type: 'Romana', weight: 5, length: 10 },
-  { type: 'Romana', weight: 7, length: 9 },
-  { type: 'Trombetta', weight: 3, length: 8 },
-  { type: 'Calabrese', weight: 6, length: 27 },
-  { type: 'Calabrese', weight: 14, length: 4 },
+  { type: "Napoletana", weight: 10, length: 4 },
+  { type: "Trombetta", weight: 13, length: 16 },
+  { type: "Napoletana", weight: 4, length: 23 },
+  { type: "Trombetta", weight: 11, length: 6 },
+  { type: "Napoletana", weight: 2, length: 17 },
+  { type: "Romana", weight: 5, length: 10 },
+  { type: "Romana", weight: 7, length: 9 },
+  { type: "Trombetta", weight: 3, length: 8 },
+  { type: "Calabrese", weight: 6, length: 27 },
+  { type: "Calabrese", weight: 14, length: 4 },
 ];
 
 // A partire dall'array fornito, crea due array. Uno con le zucchine più lunghe di almeno 15cm. L'altro con le restanti.
+
+//ciclo FOR
+const xl = [];
+
+const normal = [];
+
+for (let i = 0; i < zucchine.length; i++) {
+  const zucchINE = zucchine[i];
+  if (zucchINE.length > 15) {
+    xl.push(zucchINE);
+  } else {
+    zucchINE < 15;
+    normal.push(zucchINE);
+  }
+}
+
+console.log(xl);
+console.log(normal);
+
+//ciclo forEACH
